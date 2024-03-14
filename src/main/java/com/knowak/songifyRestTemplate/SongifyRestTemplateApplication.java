cee98e1355e72cf27e38db23a87de6e3c9851bfb
Krzysztof Nowak <krzyqq@gmail.com>
2024-03-14 14:15:44 +0100

    Initial commit

---
+++ src/main/java/com/knowak/songifyRestTemplate/SongifyRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.knowak.songifyRestTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongifyRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowak/songifyRestTemplate/SongifyRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowak.songifyRestTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongifyRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
